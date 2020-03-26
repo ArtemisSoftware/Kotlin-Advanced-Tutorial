37dee3d12c1b1417a3bb894a1699f9dafcfc5c87
gustavo <gustavo.maia@b2b.com.pt>
2020-03-26 11:04:31 +0000

    Sealed Classes

---
+++ src/main/kotlin/com/artemis/software/kotlinadvanced/classes/AlgebraicDataTypes.kt
@@ -0,0 +1,30 @@
+package com.artemis.software.kotlinadvanced.classes
+
+sealed class PageResult{
+    class Success(val content: String): PageResult()
+    class Error(val code: Int, val message: String): PageResult()
+}
+
+fun getURLPage(url: String): PageResult{
+
+    val wasValidCall = false
+
+    if(wasValidCall){
+        return PageResult.Success("The content")
+    }
+    else{
+        return PageResult.Error(404,"Not found")
+    }
+}
+
+
+fun main(args: Array<String>) {
+
+    val pageResult = getURLPage("/")
+
+    when(pageResult){
+        is PageResult.Success -> println(pageResult.content)
+        is PageResult.Error -> println(pageResult.message)
+    }
+
+}
