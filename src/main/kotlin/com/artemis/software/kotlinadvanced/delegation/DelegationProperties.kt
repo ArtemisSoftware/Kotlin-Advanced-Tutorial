e037c5833de2c735982b563de9e68cb970707195
gustavo <gustavo.maia@b2b.com.pt>
2020-04-02 11:38:35 +0100

    Delegating Properties

---
+++ src/main/kotlin/com/artemis/software/kotlinadvanced/delegation/DelegationProperties.kt
@@ -0,0 +1,38 @@
+package com.artemis.software.kotlinadvanced.delegation
+
+import kotlin.reflect.KProperty
+
+class Service{
+
+    var someProperty: String by ExternalFunctionality()
+}
+
+
+class ExternalFunctionality{
+
+    var backingField = "Default"
+
+    operator fun getValue(service: Service, property: KProperty<*>): String {
+        println("getValue called with params: \n" +
+                "service: $service\n" +
+                "property: ${property.name}"
+        )
+
+        return backingField;
+    }
+
+    operator fun setValue(service: Service, property: KProperty<*>, s: String) {
+        backingField = s
+    }
+
+
+}
+
+fun main(args: Array<String>) {
+
+    val service = Service()
+
+    println(service.someProperty)
+    service.someProperty = "New Value"
+    println(service.someProperty)
+}
